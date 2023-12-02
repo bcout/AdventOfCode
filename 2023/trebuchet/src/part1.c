038e1b077aa43f7f761835a72b86deb1d0ad8cab
Brennan C <brennancout@gmail.com>
2023-12-02 14:32:02 -0400

    Error framework causing segfaults, gonna remove it, this commit has the code for the error stuff

--- 2023/trebuchet/src/part1.c
+++ 2023/trebuchet/src/part1.c
@@ -3,10 +3,10 @@
 #include "../include/utils.h"
 
 void run_part1(){
-    char* test_input = "../data/part1test.txt";
+    char* test_input = "/home/brennan/Documents/AdventOfCode/2023/trebuchet/data/part1test.txt";
     char* input = "../data/part1.txt";
 
-    char* file = test_input;
+    char* file = input;
 
     char** input_lines = read_input_file(file);
     
--- 2023/trebuchet/src/utils.c
+++ 2023/trebuchet/src/utils.c
@@ -17,33 +17,45 @@ char* str_concat(char* s1, char* s2){
     char* result = malloc(strlen(s1) + strlen(s2) + 1);
     if (result == NULL){
         print_error_and_exit("str_concat()", "malloc() failed", 0);
-        exit(EXIT_FAILURE); 
     }
-    strcpy(result, s1);
-    strcat(result, s2);
+    strcpy(strcpy(result, s1), s2);
     return result;
 }
 
 FILE* open_file(char* file_path){
+    printf("Hel,lo\n");
+    printf("%s\n", file_path);
+
+    printf("%s", strcat("Could not open ", file_path));
+    
     FILE* fp = fopen(file_path, "r");
     if (fp == NULL){
-        print_error_and_exit("open_file()", strcat("Could not open ", file_path), 0);
+        //print_error_and_exit("open_file()", strcat("Could not open ", file_path), 1);
     }
     return fp;
+    
 }
 
 long get_filesize(FILE* fp){
     if (fseek(fp, 0, SEEK_END) != 0) {
         print_error_and_exit("get_filesize()", "fseek() failed", 0);
     } 
-    return 0;
+    long filesize = ftell(fp);
+    if (filesize < 1){
+        print_error_and_exit("get_filesize()", "filesize is less than 1", 0);
+    }
+
+    rewind(fp);
+    return filesize;
 }
 
 char** read_input_file(char* file_path){
     printf("File path: %s\n", file_path);
-
-    //FILE* fp = open_file(file_path);
+    printf("hello");
+    FILE* fp = open_file(file_path);
     //long filesize = get_filesize(fp);
+
+    //printf("%lu", filesize);
 
     void* line_v = malloc(INITIAL_BUFFER_SIZE * sizeof(char));
     if (line_v == NULL){
